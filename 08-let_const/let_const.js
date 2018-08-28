db62119f6ef961dede70d935ae7f9e3d5c3a0069
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-08-28 15:23:14 +0530

    08-let_const

---
+++ 08-let_const/let_const.js
@@ -0,0 +1,8 @@
+var myName = 'Ajay';
+console.log(myName);
+myName = 'Raj';
+console.log(myName);
+var place = 'TVM';
+console.log(place);
+place = 'KLM';
+console.log(place);
---
+++ 08-let_const/let_const.ts
@@ -0,0 +1,9 @@
+var myName = 'Ajay';
+console.log(myName);
+
+myName = 'Raj';
+console.log(myName);
+
+const place = 'TVM';
+//place = 'KLM';
+console.log(place);
