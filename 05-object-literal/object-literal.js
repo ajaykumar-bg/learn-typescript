7b1f845050ceac10ec82a871ade0e4144ef62870
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-08-28 15:13:15 +0530

    05-object-literal

---
+++ 05-object-literal/object-literal.js
@@ -0,0 +1,7 @@
+var pizza = {
+    name: 'Cheese Burger',
+    price: 75
+};
+var toppings = ['cheese'];
+var order = { pizza: pizza, toppings: toppings };
+console.log(order);
---
+++ 05-object-literal/object-literal.ts
@@ -0,0 +1,10 @@
+const pizza = {
+    name: 'Cheese Burger',
+    price: 75
+}
+
+const toppings = ['cheese'];
+
+const order = {pizza, toppings}
+
+console.log(order);
