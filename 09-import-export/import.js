b0153de900c22ebfbf47005e9451a033c6fd8e5b
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-08-28 15:32:20 +0530

    09-import-export

---
+++ 09-import-export/import.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var export_1 = require("./export");
+var pizza = 'Pepperoni';
+console.log(export_1.uppercase(pizza));
---
+++ 09-import-export/import.ts
@@ -0,0 +1,5 @@
+import { uppercase } from './export';
+
+const pizza = 'Pepperoni';
+
+console.log(uppercase(pizza));
