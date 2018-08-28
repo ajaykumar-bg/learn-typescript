3c5fbe545fe73bee241afdf8fa00861bd31907fe
Ajay Kumar BG <ajaykumar.bg@perfomatix.com>
2018-08-28 18:52:49 +0530

    25-enum-numeric

---
+++ 25-enum-numeric/enum-numeric.ts
@@ -0,0 +1,14 @@
+enum Sizes {
+  Small,
+  Medium,
+  Large,
+}
+enum Sizes {
+  ExtraLarge = 3,
+}
+
+const selectedSize = 1;
+
+console.log("Sizes",Sizes);
+console.log("Sizes.Large",Sizes.Medium);
+console.log("Sizes[selectedSize]",Sizes[selectedSize]);
